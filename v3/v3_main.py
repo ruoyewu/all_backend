8e10bd97b4c76142010c983dd4ec2d8f0945e5cb
张群 <245592918@qq.com>
2017-10-26 16:24:27 +0800

    增加 数字尾巴

--- v3/v3_main.py
+++ v3/v3_main.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 
 from v3 import v3_list, v3_detail, v3_sql, v3_const
 
-file_dir = 'C:\\Users\\Administrator\\Desktop\\allapp\\files\\avatar'
+file_dir = '/home/ubuntu/project/all_python/file/avatar/'
 
 v3_app = Blueprint('v3', __name__, url_prefix='/v3')
 
