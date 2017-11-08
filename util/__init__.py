de34f11a8dd7b668c6810aa08b580dae2fa828dd
张群 <245592918@qq.com>
2017-11-08 09:24:30 +0800

    fa

--- util/__init__.py
+++ util/__init__.py
@@ -1,7 +1,8 @@
 from Crypto.PublicKey import RSA
 
 
-file = open('/Users/wuruoye/Documents/python/all_app/util/private_rsa.pem')
+# file = open('/Users/wuruoye/Documents/python/all_app/util/private_rsa.pem')
+file = open('/home/ubuntu/project/all/all_python/util/private_rsa.pem')
 content = file.read()
 file.close()
 
