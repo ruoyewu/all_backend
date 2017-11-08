4e401b2382c90aa3bd0fcf81eeeb1901fbc9e4f4
张群 <245592918@qq.com>
2017-11-08 14:44:43 +0800

    test

--- util/secret_util.py
+++ util/secret_util.py
@@ -1,15 +1,17 @@
 from Crypto import Random
 from Crypto.Cipher import PKCS1_v1_5
-import util
+from Crypto.PublicKey import RSA
 import base64
 
 random_generator = Random.new().read
 
 
 def decrypt_rsa(secret):
-    cipher = PKCS1_v1_5.new(util.private_key)
-    text = cipher.decrypt(base64.b64decode(secret), random_generator)
-    return text.decode('utf-8')
+    with open('/home/ubuntu/project/all/all_test/all_python/util/private_rsa.pem') as file:
+        private_key = RSA.importKey(file.read())
+        cipher = PKCS1_v1_5.new(private_key)
+        text = cipher.decrypt(base64.b64decode(secret), random_generator)
+        return text.decode('utf-8')
 
 
 if __name__ == "__main__":
