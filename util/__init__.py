aeaaadc7769964f92c8acbd4f9def98868a68734
张群 <245592918@qq.com>
2017-11-08 08:51:22 +0800

    secret test

--- util/__init__.py
+++ util/__init__.py
@@ -1,10 +1,5 @@
-import base64
-
-from Crypto import Random
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
 
-random_generator = Random.new().read
 
 file = open('private_rsa.pem')
 content = file.read()
@@ -12,10 +7,3 @@ file.close()
 
 private_key = RSA.importKey(content)
 
-
-def decrypt_rsa(secret):
-    cipher = PKCS1_v1_5.new(private_key)
-    print(secret)
-    text = cipher.decrypt(base64.b64decode(secret), random_generator)
-    print(text)
-    return text.decode()
---
+++ util/secret.py
@@ -0,0 +1,17 @@
+from Crypto import Random
+from Crypto.Cipher import PKCS1_v1_5
+from util import private_key
+import base64
+
+random_generator = Random.new().read
+
+
+def decrypt_rsa(secret):
+    cipher = PKCS1_v1_5.new(private_key)
+    text = cipher.decrypt(base64.b64decode(secret), random_generator)
+    return text.decode()
+
+
+if __name__ == "__main__":
+    message = 'xviaum2UGVRtLBAeHVYUlft/82K2WoIFxDcKT4FnbRZ2ptEISX3D+yRQcuYg5DOQEeE3MeTbQ04r\ny436cpnqjeq6qPvVEVvejEmNoTosvwp0AVvMBaYNj6TkGL78GoleUcFxTA6p7w9SP1Vsc0bRcpi7\nF1qUfFXQBliUFY5UEaI='
+    print(decrypt_rsa(message))
